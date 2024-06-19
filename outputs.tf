3d96c2e3bf3e532f097955b3238aa285c084ee9f
Ravi Prakash <129740581+raviiai@users.noreply.github.com>
2024-06-19 22:08:25 +0530

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+
+output "instance_ip" {
+  value = aws_instance.strapi_instance.public_ip
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "ami" {
+  default = "ami-053a617c6207ecc7b"
+}

70d247d7c606cb55a42fdc2f3bbdabf3092ffa93
ravi <raviiprakash.ai@gmail.com>
2024-06-20 10:14:47 +0530

    deleted files

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-
-output "instance_ip" {
-  value = aws_instance.strapi_instance.public_ip
-}
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {
-  default = "eu-west-2"
-}
-
-variable "ami" {
-  default = "ami-053a617c6207ecc7b"
-}
